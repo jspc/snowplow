9a45f25aafaaa45103c407a2131f9c8800f9d811
Ben Fradet <benjamin.fradet@gmail.com>
2018-02-20 13:16:18 +0000

    Scala Stream Collector: split into multiple artifacts according to targeted platform (closes #3621)

---
+++ 2-collectors/scala-stream-collector/pubsub/src/main/scala/com.snowplowanalytics.snowplow.collectors.scalastream/PubSubCollector.scala
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2013-2017 Snowplow Analytics Ltd. All rights reserved.
+ *
+ * This program is licensed to you under the Apache License Version 2.0, and
+ * you may not use this file except in compliance with the Apache License
+ * Version 2.0.  You may obtain a copy of the Apache License Version 2.0 at
+ * http://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the Apache License Version 2.0 is distributed on an "AS
+ * IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied.  See the Apache License Version 2.0 for the specific language
+ * governing permissions and limitations there under.
+ */
+package com.snowplowanalytics.snowplow
+package collectors
+package scalastream
+
+import scalaz._
+import Scalaz._
+
+import model._
+import sinks.PubSubSink
+
+object PubSubCollector extends App with Collector {
+  val (collectorConf, akkaConf) = parseConfig(args)
+
+  val sinks = for {
+    pc <- collectorConf.streams.sink match {
+      case pc: PubSub => pc.right
+      case _ => new IllegalArgumentException("Configured sink is not PubSub").left
+    }
+    goodStream = collectorConf.streams.good
+    badStream = collectorConf.streams.bad
+    bufferConf = collectorConf.streams.buffer
+    good <- PubSubSink.createAndInitialize(pc, bufferConf, goodStream)
+    bad <- PubSubSink.createAndInitialize(pc, bufferConf, badStream)
+  } yield CollectorSinks(good, bad)
+
+  sinks match {
+    case \/-(s) => run(collectorConf, akkaConf, s)
+    case -\/(e) => throw e
+  }
+}
--- 2-collectors/scala-stream-collector/pubsub/src/main/scala/com.snowplowanalytics.snowplow.collectors.scalastream/sinks/PubSubSink.scala
+++ 2-collectors/scala-stream-collector/pubsub/src/main/scala/com.snowplowanalytics.snowplow.collectors.scalastream/sinks/PubSubSink.scala
@@ -16,8 +16,7 @@ package scalastream
 package sinks
 
 import scala.collection.JavaConverters._
-import scala.util.Try
-import scala.util.control.NonFatal
+import scala.util.{Failure, Success, Try}
 
 import com.google.api.core.{ApiFutureCallback, ApiFutures}
 import com.google.api.gax.batching.BatchingSettings
@@ -27,64 +26,84 @@ import com.google.cloud.pubsub.v1.{Publisher, TopicAdminClient}
 import com.google.pubsub.v1.{ProjectName, PubsubMessage, TopicName}
 import com.google.protobuf.ByteString
 import org.threeten.bp.Duration
+import scalaz._
+import Scalaz._
 
 import model._
 
-/**
- * PubSub Sink for the Scala collector
- */
-class PubSubSink(
-  pubSubConfig: PubSub,
-  bufferConfig: BufferConfig,
-  topicName: String
-) extends Sink {
-
-  // maximum size of a pubsub message is 10Mb
-  override val MaxBytes: Long = 10000000L
-
-  private val batchingSettings: BatchingSettings =
-    BatchingSettings.newBuilder()
-      .setElementCountThreshold(bufferConfig.recordLimit)
-      .setRequestByteThreshold(bufferConfig.byteLimit)
-      .setDelayThreshold(Duration.ofMillis(bufferConfig.timeLimit))
-      .build()
-
-  private val retrySettings: RetrySettings =
-    RetrySettings.newBuilder()
-      .setInitialRetryDelay(Duration.ofMillis(pubSubConfig.backoffPolicy.minBackoff))
-      .setMaxRetryDelay(Duration.ofMillis(pubSubConfig.backoffPolicy.maxBackoff))
-      .setRetryDelayMultiplier(pubSubConfig.backoffPolicy.multiplier)
-      .setTotalTimeout(Duration.ofMillis(pubSubConfig.backoffPolicy.totalBackoff))
-      .build()
-
-  private val publisher = createPublisher()
-  require(publisher.isRight, s"Couldn't create publisher ${publisher.left.map(_.getMessage)}")
-  require(topicExists(topicName), s"PubSub topic $topicName doesn't exist")
+/** PubSubSink companion object with factory method */
+object PubSubSink {
+  def createAndInitialize(
+    pubSubConfig: PubSub,
+    bufferConfig: BufferConfig,
+    topicName: String
+  ): \/[Throwable, PubSubSink] = for {
+    batching <- batchingSettings(bufferConfig).right
+    retry = retrySettings(pubSubConfig.backoffPolicy)
+    publisher <- createPublisher(pubSubConfig.googleProjectId, topicName, batching, retry) match {
+      case Success(p) => p.right
+      case Failure(t) => t.left
+    }
+    topicExists <- topicExists(pubSubConfig.googleProjectId, topicName)
+      .flatMap { b =>
+        if (b) b.right
+        else new IllegalArgumentException(s"PubSub topic $topicName doesn't exist").left
+      }
+  } yield new PubSubSink(publisher, topicName)
 
   /**
    * Instantiates a Publisher on an existing topic with the given configuration options.
    * This can fail if the publisher can't be created.
    * @return a PubSub publisher or an error
    */
-  private def createPublisher(): Either[Throwable, Publisher] =
-    try {
-      val p = Publisher.newBuilder(TopicName.of(pubSubConfig.googleProjectId, topicName))
-        .setBatchingSettings(batchingSettings)
-        .setRetrySettings(retrySettings)
-        .build()
-      Right(p)
-    } catch {
-      case NonFatal(e) => Left(e)
-    }
+  private def createPublisher(
+    projectId: String,
+    topicName: String,
+    batchingSettings: BatchingSettings,
+    retrySettings: RetrySettings
+  ): Try[Publisher] =
+    Try(Publisher.newBuilder(TopicName.of(projectId, topicName))
+      .setBatchingSettings(batchingSettings)
+      .setRetrySettings(retrySettings)
+      .build())
 
-  private def topicExists(topicName: String): Boolean = (for {
-    topicAdminClient <- Try(TopicAdminClient.create()).toOption
-    topics <-
-      Try(topicAdminClient.listTopics(ProjectName.of(pubSubConfig.googleProjectId)))
-        .map(_.iterateAll.asScala.toList)
-        .toOption
+  private def batchingSettings(bufferConfig: BufferConfig): BatchingSettings =
+    BatchingSettings.newBuilder()
+      .setElementCountThreshold(bufferConfig.recordLimit)
+      .setRequestByteThreshold(bufferConfig.byteLimit)
+      .setDelayThreshold(Duration.ofMillis(bufferConfig.timeLimit))
+      .build()
+
+  private def retrySettings(backoffPolicy: BackoffPolicyConfig): RetrySettings =
+    RetrySettings.newBuilder()
+      .setInitialRetryDelay(Duration.ofMillis(backoffPolicy.minBackoff))
+      .setMaxRetryDelay(Duration.ofMillis(backoffPolicy.maxBackoff))
+      .setRetryDelayMultiplier(backoffPolicy.multiplier)
+      .setTotalTimeout(Duration.ofMillis(backoffPolicy.totalBackoff))
+      .build()
+
+  /** Checks that a PubSub topic exists **/
+  private def topicExists(projectId: String, topicName: String): \/[Throwable, Boolean] = for {
+    topicAdminClient <- Try(TopicAdminClient.create()) match {
+      case Success(tac) => tac.right
+      case Failure(t) => t.left
+    }
+    topics <- Try(topicAdminClient.listTopics(ProjectName.of(projectId)))
+      .map(_.iterateAll.asScala.toList) match {
+        case Success(topics) => topics.right
+        case Failure(t) => t.left
+      }
     exists = topics.map(_.getName).contains(topicName)
-  } yield exists).getOrElse(false)
+  } yield exists
+}
+
+/**
+ * PubSub Sink for the Scala collector
+ */
+class PubSubSink private (publisher: Publisher, topicName: String) extends Sink {
+
+  // maximum size of a pubsub message is 10Mb
+  override val MaxBytes: Long = 10000000L
 
   /**
    * Convert event bytes to a PubsubMessage to be published
